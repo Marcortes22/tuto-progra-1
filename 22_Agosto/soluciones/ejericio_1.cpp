590d4fd5da75d29f7b513659d443fa8f90fa9bab
Marco Cortes <marcortes.stiven@gmail.com>
2025-08-23 12:26:13 -0600

    solve exercice 1

--- 22_Agosto/soluciones/ejericio_1.cpp
+++ 22_Agosto/soluciones/ejericio_1.cpp
@@ -2,7 +2,17 @@
 #include <iostream>  
 using namespace std; 
 
+int sumar(int a, int b) {
+    return a + b;
+}
+
 int main() {
-    cout << "Hola, mundo!" << endl;
+   int num1, num2;
+   cout << "Ingrese el primer numero: ";
+   cin >> num1;
+   cout << "Ingrese el segundo numero: ";
+   cin >> num2;
+   int resultado = sumar(num1, num2);
+   cout << "La suma es: " << resultado << endl;
     return 0;
 }
